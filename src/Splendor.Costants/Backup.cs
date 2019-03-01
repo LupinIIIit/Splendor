a758f8b693642deb7c3e098543c5e461d9f54add
Antonio Narra <tony@fatture-online.com>
2019-03-01 14:59:07 +0100

    moved costants

---
+++ src/Splendor.Costants/Backup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Splendor.Costants {
+    public static class Backup {
+        public const string SECT_OUT_FOLDER = "output-folder";
+        public const string SECT_PATH = "path";
+        public const string SECT_CREDENTIAL = "network-credential";
+        public const string USER_NAME = "user-name";
+        public const string DOMAIN = "domain";
+        public const string PASSWORD = "passord";
+        public const string MAX_FILE_SIZE = "max-file-size";
+
+        public const string DEFAULT_DOMAIN = "microsoftlocal";
+        //test comment fo git
+    }
+}
